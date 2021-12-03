4b32b29ff546652e6d7b9a677ff415e0eb82b5a0
abraker <abraker95@gmail.com>
2021-12-03 10:20:02 -0500

    Move mode options to config file
    Use `if __name__ == '__main__':` and `class Main()` paradigme
    Seperate mode code into their own functions

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 
 
 class Config():
-    
-    plots = False  # Whether to plot stuff 
-    wcsv  = True  # Whether to write results into csv
+
+    MODE_RANKED      = 0
+    MODE_COLLECTIONS = 1
+
+    plots = True        # Whether to plot stuff 
+    wcsv  = True         # Whether to write results into csv
+    mode  = MODE_RANKED
