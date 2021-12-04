c65c21126f8fcd71778a0cf3c3cb8cfdd8f5dd5e
Kominaru <j.ruza@udc.es>
2021-12-04 02:03:46 +0100

    Incorporate Rhythm model

--- utils/parser.py
+++ utils/parser.py
@@ -11,6 +11,8 @@ from modules.Inverse import obtainInverseCalculation
 from modules.Release import obtainReleaseCalculation
 from modules.Strain import obtainStrainCalculation
 from modules.Hold import obtainHoldCalculation
+from modules.Rhythm import obtainRhythmCalculation
+
 
 class HitObject:
     def __init__(self, column, timestamp, lnend=0) -> None:
@@ -28,75 +30,86 @@ class Beatmap:
         self.beatmapid = beatmapid
         self.keys = keys
 
+
 class BeatmapCalculations:
-    def __init__(self,b):
-        ho=b.hitobjects
-        ho_dt=b.dt_hitobjects
-        self.nomod=ModuleCalculations(ho)
-        self.dt=ModuleCalculations(ho_dt)
+    def __init__(self, b):
+        ho = b.hitobjects
+        ho_dt = b.dt_hitobjects
+        self.nomod = ModuleCalculations(ho)
+        self.dt = ModuleCalculations(ho_dt)
 
 
 class ModuleCalculations:
-    def __init__(self,ho):
+    def __init__(self, ho):
 
-        t=time()
-        #Basic Module calculations
+        t = time()
+        # Basic Module calculations
         self.dns = obtainDensityCalculation(ho)
         # print(f"Density: {time()-t}")
-        t0=time()
+        t0 = time()
         self.stn = obtainStrainCalculation(ho)
         # print(f"Strain: {time()-t0}")
-        t0=time()
-       
+        t0 = time()
+
         self.inv = obtainInverseCalculation(ho)
         # print(f"Inverse: {time()-t0}")
-        t0=time()
+        t0 = time()
         self.rel = obtainReleaseCalculation(ho)
         # print(f"Release: {time()-t0}")
-        t0=time()
+        t0 = time()
         self.lns = obtainLNnessCalculation(ho)
         # print(f"LNNess: {time()-t0}")
-        t0=time()
+        t0 = time()
         self.hld = obtainHoldCalculation(ho)
         # print(f"Hold: {time()-t0}")
-        t0=time()
+        t0 = time()
         self.mnp = obtainManipCalculation(ho)
         # print(f"Manip: {time()-t0}")
-        t0=time()
+        t0 = time()
         # print(f"Basic Modules: {time()-t}")
-        t=time()
+        t = time()
 
-        #Rolling averages
-        self.dns_roll=roll(self.dns)
-        self.mnp_roll=roll(self.mnp)
-        self.stn_roll=roll(self.stn)
-        self.inv_roll=roll(self.inv)
-        self.rel_roll=roll(self.rel)
-        self.lns_roll=roll(self.lns)
-        self.hld_roll=roll(self.hld)
+        self.rhy = obtainRhythmCalculation(ho)
+
+        # Rolling averages
+        self.dns_roll = roll(self.dns)
+        self.mnp_roll = roll(self.mnp)
+        self.stn_roll = roll(self.stn)
+        self.inv_roll = roll(self.inv)
+        self.rel_roll = roll(self.rel)
+        self.lns_roll = roll(self.lns)
+        self.hld_roll = roll(self.hld)
+        self.rhy_roll = roll(self.rhy)
         # print(f"Rolling avgs: {time()-t}")
-        t=time()
-        #Rice Total
-        self.rice_ttl=self.computeRiceTotal(self.dns,self.mnp,self.stn)
-        self.rice_ttl_roll=self.computeRiceTotal(self.dns_roll,self.mnp_roll,self.stn_roll)
+        t = time()
+        # Rice Total
+        self.rice_ttl = self.computeRiceTotal(
+            self.dns, self.mnp, self.stn, self.rhy)
+        self.rice_ttl_roll = self.computeRiceTotal(
+            self.dns_roll, self.mnp_roll, self.stn_roll, self.rhy_roll)
 
-        #Ln Total
-        self.ln_ttl=self.computeLNTotal(self.inv,self.rel,self.lns,self.hld)
-        self.ln_ttl_roll=self.computeLNTotal(self.inv_roll,self.rel_roll,self.lns_roll,self.hld_roll)
+        # Ln Total
+        self.ln_ttl = self.computeLNTotal(
+            self.inv, self.rel, self.lns, self.hld)
+        self.ln_ttl_roll = self.computeLNTotal(
+            self.inv_roll, self.rel_roll, self.lns_roll, self.hld_roll)
 
-        #Global
-        self.ttl=self.computeGlobal(self.rice_ttl,self.ln_ttl)
-        self.ttl_roll=self.computeGlobal(self.rice_ttl_roll,self.ln_ttl_roll)
+        # Global
+        self.ttl = self.computeGlobal(self.rice_ttl, self.ln_ttl)
+        self.ttl_roll = self.computeGlobal(
+            self.rice_ttl_roll, self.ln_ttl_roll)
         # print(f"Globals: {time()-t}")
-        t=time()
-    def computeRiceTotal(self,dns,mnp,stn):
-        return (dns/mnp)*stn
-    
-    def computeLNTotal(self,inv,rel,lns,hld):
-        return np.power(1+inv+2*rel,lns)*np.power(hld,1)
+        t = time()
 
-    def computeGlobal(self,rice,ln):
-        return rice * np.power(ln,1)
+    def computeRiceTotal(self, dns, mnp, stn, rhy):
+        return (dns/mnp)*stn*np.power(rhy, 0.7)
+
+    def computeLNTotal(self, inv, rel, lns, hld):
+        return np.power(1+inv+2*rel, lns)*np.power(hld, 1)
+
+    def computeGlobal(self, rice, ln):
+        return rice * np.power(ln, 1)
+
 
 def obtainHitObjectArrayFromOsu(file):
     file.seek(0)
@@ -147,7 +160,7 @@ def obtainHitObjectArrayFromOsu(file):
         hitobjects.append(HitObject(column, timestamp, lnend))
         dt_hitobjects.append(HitObject(column, timestamp//1.5, lnend//1.5))
     b = Beatmap(title, artist, creator, version,
-                    hitobjects, beatmapid, keys, dt_hitobjects)
+                hitobjects, beatmapid, keys, dt_hitobjects)
     return b
 
 
@@ -168,6 +181,4 @@ w = 50
 def roll(a):
     # return np.array([np.average(a[max(0, i-w//2):min(len(a), i+w//2)]) for i in range(len(a))])
     a_padded = np.pad(a, (w//2, w-1-w//2), mode='edge')
-    return np.convolve(a_padded, np.ones((w,))/w, mode='valid') 
-
-    
+    return np.convolve(a_padded, np.ones((w,))/w, mode='valid')
